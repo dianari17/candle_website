e594a674002440a0cc4afc95662f8bc7e39bb69b
Natalie Teaman <natalieteaman@Mac.attlocal.net>
2024-11-22 17:02:02 -0500

    nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import TestPage from './pages/TestPage.tsx';
 import AboutPage from './pages/AboutPage.tsx';
 import Login from './pages/Login.tsx';
+import CartPage from './pages/CartPage.tsx';
+
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <Route path="/" element={<TestPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Cart" element={<CartPage />} />
       </Routes>
     </Router>
   );
