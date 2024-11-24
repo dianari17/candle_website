091dc56ef9740338c872fd47fc67c36695901e81
Christina Nguyen <christina.nguyen8711@gmail.com>
2024-11-23 20:07:54 -0500

    Implemented company story/description

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import "../assets/aboutUs";
 import TestBut from '../components/TestBut';
+import AboutUs from '../assets/aboutUs';
 const AboutPage = () => {
   return (
-    <div>
-    <TestBut/>
     <div
-    style={{
-        backgroundImage: `url('/frame.svg')`, 
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100%',
-        height: '100vh', 
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        zIndex: '-1', 
-    }}
-
-    
-></div>
-</div>
+    >
+      <TestBut />
+      <div
+        style={{
+          backgroundImage: `url('/frame.svg')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundColor: "#644934",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "100vh",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          zIndex: "-1",
+        }}
+      ></div>
+      <AboutUs />
+    </div>
   );
 };
 
