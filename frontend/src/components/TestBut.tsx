4fd37be036a8c83a0dc7933f18f1b2416d27bea6
Christina Nguyen <christina.nguyen8711@gmail.com>
2024-11-24 01:57:54 -0500

    Products Page Styling, adding products Card, Modal, Pagination
    Co-authored-by: Natalie Teaman <Nteaman@users.noreply.github.com>

--- frontend/src/components/TestBut.tsx
+++ frontend/src/components/TestBut.tsx
@@ -18,83 +18,99 @@ function moveSign(){
 function moveCart(){
     navigate('/cart')
 }
+function moveProducts() {
+  navigate('/products')
+}
 
-return(
-<div
-        style={{
-            backgroundColor: '#A48A79', // Light brown background
-            padding: '15px 0', 
-            width: '100%', 
-            position: 'fixed', // Fixed at the top
-            top: '0',
-            left: '0',
-            zIndex: '1000', // Ensure it stays above other content
-            textAlign: 'center', // Center the text items
-        }}
-        >
-            <button
-                style={{
-                    cursor: 'pointer',
-                    color: '#FFF',
-                    fontSize: '18px',
-                    margin: '0 20px',
-                    backgroundColor: '#A48A79',
-                    textDecoration: 'none',
-                    display: 'inline-block',
-                }}
-                onClick={moveAbt}
-            >
-                About
-            </button>
-            <button
-                style={{
-                    margin: '10px',
-                    padding: '10px 20px',
-                    fontSize: '16px',
-                    border: 'none',
-                    borderRadius: '5px',
-                    backgroundColor: '#A48A79',
-                    color: '#FFF',
-                    cursor: 'pointer',
-                }}
-                onClick={moveLog}
-            >
-                Log In
-            </button>
-            <button
-                style={{
-                    margin: '10px',
-                    padding: '10px 20px',
-                    fontSize: '16px',
-                    border: 'none',
-                    borderRadius: '5px',
-                    backgroundColor: '#A48A79',
-                    color: '#FFF',
-                    cursor: 'pointer',
-                }}
-                onClick={moveSign}
-            >
-                Sign Up
-            </button>
-            <button
-                style={{
-                    margin: '10px',
-                    padding: '10px 20px',
-                    fontSize: '16px',
-                    border: 'none',
-                    borderRadius: '5px',
-                    backgroundColor: '#A48A79',
-                    color: '#FFF',
-                    cursor: 'pointer',
-                }}
-                onClick={moveCart}
-            >
-                Shopping Cart
-            </button>
-            
-        </div>
-
-    );
+return (
+  <div
+    style={{
+      backgroundColor: "#A48A79", // Light brown background
+      padding: "15px 0",
+      width: "100%",
+      position: "fixed", // Fixed at the top
+      top: "0",
+      left: "0",
+      zIndex: "1000", // Ensure it stays above other content
+      textAlign: "center", // Center the text items
+    }}
+  >
+    <button
+      style={{
+        cursor: "pointer",
+        color: "#FFF",
+        fontSize: "18px",
+        margin: "0 20px",
+        backgroundColor: "#A48A79",
+        textDecoration: "none",
+        display: "inline-block",
+      }}
+      onClick={moveAbt}
+    >
+      About
+    </button>
+    <button
+      style={{
+        margin: "10px",
+        padding: "10px 20px",
+        fontSize: "16px",
+        border: "none",
+        borderRadius: "5px",
+        backgroundColor: "#A48A79",
+        color: "#FFF",
+        cursor: "pointer",
+      }}
+      onClick={moveProducts}
+    >
+      Products
+    </button>
+    <button
+      style={{
+        margin: "10px",
+        padding: "10px 20px",
+        fontSize: "16px",
+        border: "none",
+        borderRadius: "5px",
+        backgroundColor: "#A48A79",
+        color: "#FFF",
+        cursor: "pointer",
+      }}
+      onClick={moveLog}
+    >
+      Log In
+    </button>
+    <button
+      style={{
+        margin: "10px",
+        padding: "10px 20px",
+        fontSize: "16px",
+        border: "none",
+        borderRadius: "5px",
+        backgroundColor: "#A48A79",
+        color: "#FFF",
+        cursor: "pointer",
+      }}
+      onClick={moveSign}
+    >
+      Sign Up
+    </button>
+    <button
+      style={{
+        margin: "10px",
+        padding: "10px 20px",
+        fontSize: "16px",
+        border: "none",
+        borderRadius: "5px",
+        backgroundColor: "#A48A79",
+        color: "#FFF",
+        cursor: "pointer",
+      }}
+      onClick={moveCart}
+    >
+      Shopping Cart
+    </button>
+  </div>
+);
 
 }
 
