69de2c2dc1177985e1a5440cdfde243c5daefcec
Christina Nguyen <christina.nguyen8711@gmail.com>
2024-11-23 21:20:34 -0500

    MUI Template for Login/Signup

---
+++ frontend/src/components/loginForm.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { SignInPage, type AuthProvider } from "@toolpad/core/SignInPage";
+import { createTheme } from "@mui/material/styles";
+import { getDesignTokens, inputsCustomizations } from "./customTheme";
+
+const providers = [
+  { id: "github", name: "GitHub" },
+  { id: "google", name: "Google" },
+  { id: "credentials", name: "Email and Password" },
+];
+
+const signIn: (provider: AuthProvider) => void = async (provider) => {
+  const promise = new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log(`Sign in with ${provider.id}`);
+      resolve();
+    }, 500);
+  });
+  return promise;
+};
+
+export default function LoginForm() {
+  const calculatedMode = "light"; // Hardcoded to light mode
+  const brandingDesignTokens = getDesignTokens(calculatedMode);
+
+  // preview-start
+  const THEME = createTheme({
+    ...brandingDesignTokens,
+    palette: {
+      ...brandingDesignTokens.palette,
+      mode: calculatedMode,
+    },
+    components: {
+      ...inputsCustomizations,
+    },
+  });
+  // preview-end
+
+  return (
+    // preview-start
+    <AppProvider theme={THEME}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        sx={{
+          "& form > .MuiStack-root": {
+            marginTop: "2rem",
+            rowGap: "0.5rem",
+          },
+        }}
+      />
+    </AppProvider>
+    // preview-end
+  );
+}
