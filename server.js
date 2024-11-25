842c87e968a01c9ad88d9101f7aa32bbf879720c
MarkL-UCF <ma999600@ucf.edu>
2024-11-25 18:11:03 -0500

    More paypal stuff

--- server.js
+++ server.js
@@ -42,6 +42,181 @@ app.use((req, res, next) => {
     next();
 });
 
+
+function getPayPalSdkUrl() {
+    const sdkUrl = new URL("/sdk/js", PAYPAL_SDK_BASE_URL);
+    const sdkParams = new URLSearchParams({
+        "client-id": PAYPAL_CLIENT_ID,
+        components: "buttons,fastlane",
+    });
+    sdkUrl.search = sdkParams.toString();
+
+    return sdkUrl.toString();
+}
+
+//generate paypal's client token
+async function getClientToken() {
+    try {
+        if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+            throw new Error("Missing API credentials");
+        }
+
+        const url = `${PAYPAL_API_BASE_URL}/v1/oauth2/token`;
+
+        const headers = new Headers();
+        const auth = Buffer.from(
+            `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
+        ).toString("base64");
+        headers.append("Authorization", `Basic ${auth}`);
+        headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+        const searchParams = new URLSearchParams();
+        searchParams.append("grant_type", "client_credentials");
+        searchParams.append("response_type", "client_token");
+        searchParams.append("intent", "sdk_init");
+        searchParams.append("domains[]", DOMAINS);
+
+        const options = {
+            method: "POST",
+            headers,
+            body: searchParams,
+        };
+
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        return data.access_token;
+    } catch (error) {
+        console.error(error);
+        return "";
+    }
+} 
+
+async function renderCheckout(req, res) {
+    const sdkUrl = getPayPalSdkUrl();
+    const clientToken = await getClientToken();
+    const locals = {
+        title: "Fastlane - PayPal Integration Quick Start",
+        prerequisiteScripts: `
+        <script
+          src="${sdkUrl}"
+          data-sdk-client-token="${clientToken}"
+          defer
+        ></script>
+      `,
+        initScriptPath: "app.js",
+        stylesheetPath: "./styles.css",
+    };
+
+    res.render("checkout", locals);
+}
+
+async function getAccessToken() {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+        throw new Error("Missing API credentials");
+    }
+
+    const url = `${PAYPAL_API_BASE_URL}/v1/oauth2/token`;
+
+    const headers = new Headers();
+    const auth = Buffer.from(
+        `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
+    ).toString("base64");
+    headers.append("Authorization", `Basic ${auth}`);
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const searchParams = new URLSearchParams();
+    searchParams.append("grant_type", "client_credentials");
+
+    const options = {
+        method: "POST",
+        headers,
+        body: searchParams,
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data.access_token;
+}
+
+//create and confirm order
+export async function createOrder(req, res) {
+    try {
+        const { paymentToken, shippingAddress } = req.body;
+
+        const url = `${PAYPAL_API_BASE_URL}/v2/checkout/orders`;
+
+        const headers = new Headers();
+        const accessToken = await getAccessToken();
+        headers.append("PayPal-Request-Id", Date.now().toString());
+        headers.append("Authorization", `Bearer ${accessToken}`);
+        headers.append("Content-Type", "application/json");
+
+        const { fullName } = shippingAddress?.name ?? {};
+        const { countryCode, nationalNumber } =
+            shippingAddress?.phoneNumber ?? {};
+
+        const payload = {
+            intent: "CAPTURE",
+            payment_source: {
+                card: {
+                    single_use_token: paymentToken.id,
+                },
+            },
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: "USD",
+                        value: "100",
+                    },
+                    ...(shippingAddress && {
+                        shipping: {
+                            type: "SHIPPING",
+                            ...(fullName && {
+                                name: {
+                                    full_name: fullName,
+                                },
+                            }),
+                            address: {
+                                address_line_1:
+                                    shippingAddress.address.addressLine1,
+                                address_line_2:
+                                    shippingAddress.address.addressLine2,
+                                admin_area_2:
+                                    shippingAddress.address.adminArea2,
+                                admin_area_1:
+                                    shippingAddress.address.adminArea1,
+                                postal_code: shippingAddress.address.postalCode,
+                                country_code:
+                                    shippingAddress.address.countryCode,
+                            },
+                            ...(countryCode &&
+                                nationalNumber && {
+                                    phone_number: {
+                                        country_code: countryCode,
+                                        national_number: nationalNumber,
+                                    },
+                                }),
+                        },
+                    }),
+                },
+            ],
+        }; 
+        const response = await fetch(url, {
+            method: "POST",
+            headers,
+            body: JSON.stringify(payload),
+        });
+        const result = await response.json();
+
+        res.status(response.status).json({ result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+} 
+
 // Returns true if the token specifies an admin user
 async function isAdmin(token) {
     try {
@@ -63,52 +238,6 @@ function getID(token) {
         return false;
     }
 }
-
-//generate paypal's client token
-async function getClientToken() {
-
-    try {
-
-        if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
-
-            throw new Error("Missing API credentials");
-
-        }
-
-        const url = `${PAYPAL_API_BASE_URL}/v1/oauth2/token`;
-        const headers = new Headers();
-
-        const auth = Buffer.from(
-
-            `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
-
-        ).toString("base64");
-
-        headers.append("Authorization", `Basic ${auth}`);
-        headers.append("Content-Type", "application/x-www-form-urlencoded");
-
-        const searchParams = new URLSearchParams();
-        searchParams.append("grant_type", "client_credentials");
-        searchParams.append("response_type", "client_token");
-        searchParams.append("intent", "sdk_init");
-        searchParams.append("domains[]", DOMAINS);
-
-        const options = {
-
-            method: "POST",
-            headers,
-            body: searchParams,
-
-        };
-        const response = await fetch(url, options);
-        const data = await response.json();
-        return data.access_token;
-
-    } catch (error) {
-        console.error(error);
-        return "";
-    }
-} 
 
 app.post('/api/addProduct', upload.single('productImage'), async (req, res, next) => {
 
